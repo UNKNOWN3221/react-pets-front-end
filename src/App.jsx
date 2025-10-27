29caee2c5fac1439c8016e9d3116276a5948b973
ahq_292 <ali.quraishy2014@email.com>
2025-10-27 18:42:14 +0300

    check

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,58 @@
+import { useState, useEffect } from 'react';
+import * as petService from './services/petService';
+import PetList from './components/PetList/PetList';
+import PetDetail from './components/PetDetail/PetDetail';
+import PetForm from './components/PetForm/PetForm';
 const App = () => {
-  return <h1>Hello world!</h1>;
+  const [pets, setPets] = useState([]);
+  const [selected, setSelected] = useState(null)
+  const [isFormOpen, setIsFormOpen] = useState(false);
+  // Create a new useEffect
+  useEffect(() => {
+    const fetchPets = async () => {
+      try {
+        const fetchedPets = await petService.index();
+        // Don't forget to pass the error object to the new Error
+        if (fetchedPets.err) {
+          throw new Error(fetchedPets.err);
+        }
+        setPets(fetchedPets);
+      } catch (err) {
+        // Log the error object
+        console.log(err);
+      }
+    };
+    fetchPets();
+  }, []);
+const handleSelect = (pet) => {
+    setSelected(pet)
+    setIsFormOpen(false);
+  }
+  const handleFormView = () => {
+    setIsFormOpen(!isFormOpen);
+  };
+  const handleAddPet = async (formData) => {
+    try {
+      const newPet = await petService.create(formData)
+      if (newPet.err) {
+      throw new Error(newPet.err);
+    }
+      setPets([newPet, ...pets])
+      setIsFormOpen(false)
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  return (
+    <>
+    <PetList pets={pets} handleSelect={handleSelect} handleFormView={handleFormView} isFormOpen={isFormOpen}/>
+    {isFormOpen ? (
+      <PetForm handleAddPet={handleAddPet}/>
+    ):(
+    <PetDetail selected={selected} />
+    )}
+    </>
+  );
 };
 
 export default App;
---
+++ src/components/PetList/PetList.jsx
@@ -0,0 +1,33 @@
+const PetList = (props) => {
+ 
+  console.log(props);
+
+  return (
+    <div>
+      <h1>Pet List</h1>
+      <div>
+        {!props.pets.length ? (
+          <h2>No Pets Yet!</h2>
+        ) : (
+          <ul>
+          {props.pets.map((pet) => (
+            <li 
+              key={pet._id}
+              style={{ cursor: 'pointer', color: "#646CFF" }}
+              // Call the handleSelect() function on click, passing the pet.
+              onClick={() => props.handleSelect(pet)}
+            >
+              {pet.name}
+            </li>
+          ))}
+        </ul>
+        )}
+      </div>
+      <button onClick={props.handleFormView}>
+        {props.isFormOpen ? 'Close Form' : 'New Pet'}
+      </button>
+    </div>
+  );
+};
+
+export default PetList;
